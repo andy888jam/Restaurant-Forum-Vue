ccb076ce6ba8bf2a91b8fb47848b72085c7d2f07
andy888jam <andy888jam@gmail.com>
2022-03-07 20:03:41 +0800

    dashboard-and-top

---
+++ src/views/RestaurantDashboard.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="container py-5">
+    <div>
+      <h1>{{ restaurant.name }}</h1>
+      <span class="badge badge-secondary mt-1 mb-3">
+        {{ restaurant.Category.name }}
+      </span>
+    </div>
+
+    <hr />
+
+    <ul>
+      <li>評論數： {{ restaurant.Comments.length }}</li>
+      <li>瀏覽次數： {{ restaurant.viewCounts }}</li>
+    </ul>
+
+    <button type="button" class="btn btn-link" @click="$router.back()">
+      回上一頁
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      restaurant: {
+        id: 1,
+        name: "Whitney Kreiger III",
+        tel: "(960) 053-2333",
+        address: "27059 Padberg Estate",
+        opening_hours: "08:00",
+        description: "atque",
+        image:
+          "https://loremflickr.com/320/240/restaurant,food/?random=46.25370848405821",
+        viewCounts: 1,
+        createdAt: "2022-02-04T07:34:41.000Z",
+        updatedAt: "2022-02-12T15:11:24.000Z",
+        CategoryId: 1,
+        Category: {
+          id: 1,
+          name: "中式料理",
+          createdAt: "2022-02-04T07:34:41.000Z",
+          updatedAt: "2022-02-04T07:34:41.000Z",
+        },
+        Comments: [
+          {
+            id: 1,
+            text: "Vel neque debitis et alias eveniet.",
+            UserId: 2,
+            RestaurantId: 1,
+            createdAt: "2022-02-04T07:34:41.000Z",
+            updatedAt: "2022-02-04T07:34:41.000Z",
+            User: {
+              id: 2,
+              name: "user1",
+              email: "user1@example.com",
+              password:
+                "$2a$10$R2DVPzWfQ2hsSn5snqyc4eEnOO81b.Z4S10I6UDqLBxOjOFGOeRji",
+              isAdmin: false,
+              image: null,
+              createdAt: "2022-02-04T07:34:41.000Z",
+              updatedAt: "2022-02-04T07:34:41.000Z",
+            },
+          },
+          {
+            id: 51,
+            text: "In ea aperiam praesentium eum a.",
+            UserId: 3,
+            RestaurantId: 1,
+            createdAt: "2022-02-04T07:34:41.000Z",
+            updatedAt: "2022-02-04T07:34:41.000Z",
+            User: {
+              id: 3,
+              name: "user2",
+              email: "user2@example.com",
+              password:
+                "$2a$10$XdQ8ClRmBfrajA.IvGzTzOFaBthX1guuIgQgJvifFJipXjQLK55Fa",
+              isAdmin: false,
+              image: null,
+              createdAt: "2022-02-04T07:34:41.000Z",
+              updatedAt: "2022-02-04T07:34:41.000Z",
+            },
+          },
+          {
+            id: 101,
+            text: "Unde est nisi fuga.",
+            UserId: 2,
+            RestaurantId: 1,
+            createdAt: "2022-02-04T07:34:41.000Z",
+            updatedAt: "2022-02-04T07:34:41.000Z",
+            User: {
+              id: 2,
+              name: "user1",
+              email: "user1@example.com",
+              password:
+                "$2a$10$R2DVPzWfQ2hsSn5snqyc4eEnOO81b.Z4S10I6UDqLBxOjOFGOeRji",
+              isAdmin: false,
+              image: null,
+              createdAt: "2022-02-04T07:34:41.000Z",
+              updatedAt: "2022-02-04T07:34:41.000Z",
+            },
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
--- src/views/RestaurantsTop.vue
+++ src/views/RestaurantsTop.vue
@@ -9,39 +9,44 @@
     <div
       class="card mb-3"
       style="max-width: 540px;margin: auto;"
+      v-for="restaurant in restaurants" :key="restaurant.id"
     >
       <div class="row no-gutters">
         <div class="col-md-4">
           <a href="#">
             <img
               class="card-img"
-              src="https://loremflickr.com/320/240/food,dessert,restaurant/?random=2"
+              :src="restaurant.image"
             >
           </a>
         </div>
         <div class="col-md-8">
           <div class="card-body">
             <h5 class="card-title">
-              Mrs. Mckenzie Johnston
+              {{restaurant.name}}
             </h5>
-            <span class="badge badge-secondary">收藏數：0</span>
+            <span class="badge badge-secondary">收藏數：{{restaurant.FavoriteCount}}</span>
             <p class="card-text">
-              Quia pariatur perferendis architecto tenetur omnis
+              {{restaurant.description}}
             </p>
-            <a
-              href="#"
+            <router-link
+              :to="{name:'restaurant', params:{id: restaurant.id}}"
               class="btn btn-primary mr-2"
-            >Show</a>
+            >Show</router-link>
 
             <button
               type="button"
               class="btn btn-danger mr-2"
+              v-if="restaurant.isFavorited"
+              @click.stop.prevent="deleteFavorite(restaurant.id)"
             >
               移除最愛
             </button>
             <button
               type="button"
               class="btn btn-primary"
+              v-else
+              @click.stop.prevent="addFavorite(restaurant.id)"
             >
               加到最愛
             </button>
@@ -54,9 +59,213 @@
 
 <script>
 import NavTabs from './../components/NavTabs'
+const dummyData = {
+  "restaurants": [
+        {
+            "id": 50,
+            "name": "Ned Spinka",
+            "tel": "(324) 673-3716 x69533",
+            "address": "42353 Marvin Mission",
+            "opening_hours": "08:00",
+            "description": "Officia sunt sit.",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=1.4517455871850604",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 3,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        },
+        {
+            "id": 49,
+            "name": "Madge Hammes",
+            "tel": "1-164-166-8518 x026",
+            "address": "206 Kerluke Station",
+            "opening_hours": "08:00",
+            "description": "culpa",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=72.54420154302466",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 3,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        },
+        {
+            "id": 48,
+            "name": "Maryjane Parisian II",
+            "tel": "411.758.6007 x3507",
+            "address": "911 Bessie Green",
+            "opening_hours": "08:00",
+            "description": "quas",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=29.283280221069184",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 2,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        },
+        {
+            "id": 47,
+            "name": "Eve Auer",
+            "tel": "059-023-4549 x0183",
+            "address": "420 Chet Harbor",
+            "opening_hours": "08:00",
+            "description": "Sequi est vitae sint qui dolores illo doloremque e",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=97.77850731576547",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 1,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        },
+        {
+            "id": 46,
+            "name": "Idell Bergnaum",
+            "tel": "596-301-6192 x5830",
+            "address": "9918 Noah Track",
+            "opening_hours": "08:00",
+            "description": "asperiores",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=20.41891329019436",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 2,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        },
+        {
+            "id": 45,
+            "name": "Derek Green",
+            "tel": "1-867-709-3735 x483",
+            "address": "9720 Talon Lakes",
+            "opening_hours": "08:00",
+            "description": "quia aliquid ducimus",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=86.91910865210382",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 3,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        },
+        {
+            "id": 44,
+            "name": "Kim Williamson",
+            "tel": "160.908.2713 x88744",
+            "address": "473 Clemmie Square",
+            "opening_hours": "08:00",
+            "description": "aliquid consequatur non",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=12.398685598127157",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 3,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        },
+        {
+            "id": 43,
+            "name": "Oren Jacobs",
+            "tel": "966.776.9775",
+            "address": "781 Kautzer Junctions",
+            "opening_hours": "08:00",
+            "description": "Eum ut assumenda id nihil tempore dolore. Necessit",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=88.3329668432273",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 5,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        },
+        {
+            "id": 42,
+            "name": "Mr. Rico Skiles",
+            "tel": "(669) 688-7506 x62712",
+            "address": "8594 Roberts Mission",
+            "opening_hours": "08:00",
+            "description": "in",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=51.4119937255598",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 2,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        },
+        {
+            "id": 41,
+            "name": "Alford Zemlak I",
+            "tel": "495.258.7511",
+            "address": "13107 Pacocha Roads",
+            "opening_hours": "08:00",
+            "description": "modi tempore harum",
+            "image": "https://loremflickr.com/320/240/restaurant,food/?random=24.114932831713155",
+            "viewCounts": 0,
+            "createdAt": "2022-02-04T07:34:41.000Z",
+            "updatedAt": "2022-02-04T07:34:41.000Z",
+            "CategoryId": 1,
+            "FavoritedUsers": [],
+            "isFavorited": false,
+            "FavoriteCount": 0
+        }
+    ]
+}
 export default {
+  data() {
+    return {
+      restaurants: [],
+    }
+  },
   components: {
     NavTabs
+  },
+  created() {
+    this.fetchTop()
+  },
+  methods: {
+    fetchTop() {
+      const {
+        restaurants
+      } = dummyData
+      this.restaurants = restaurants
+    },
+    deleteFavorite(restaurantId) {
+      this.restaurants = this.restaurants.map(restaurant => {
+        if(restaurant.id !== restaurantId) {
+          return restaurant
+        }
+        return {
+          ...restaurant,
+          isFavorited: false,
+          FavoriteCount: restaurant.FavoriteCount - 1,
+        }
+      })
+    },
+    addFavorite(restaurantId) {
+      this.restaurants = this.restaurants.map(restaurant => {
+        if(restaurant.id !== restaurantId) {
+          return restaurant
+        }
+        return {
+          ...restaurant,
+          isFavorited: true,
+          FavoriteCount: restaurant.FavoriteCount + 1,
+        }
+      })
+    }
   }
 }
 </script>
